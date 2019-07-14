c6e2c3b47fe92370cd5411958750984d01632a17
Xavier <xtarmitage@gmail.com>
2019-07-14 18:58:32 +1000

    changed name of Grid.py file

--- Grid.py
+++ Grid.py
@@ -1,25 +1,30 @@
 #! /home/xavier/anaconda3/envs/first_env/bin/python
+
+import random
 import sys
-import numpy as np
-from collections import defaultdict
-from unionfind import Graph
-from itertools import product
+from union import Union
+
 
 class Grid:
 
     def __init__(self, size):
         self.size = size
-        self.elements = Graph(size * size)
-        self.matrix = np.around(np.random.rand(size,size), decimals=0).astype(int)
-        self.indices = list(product(range(size), repeat=2))
-        
+        self.elements = Union(size ** 2)
+        self.matrix = [[random.randint(0,1) for i in range(size)] for x in range(size)]
+        self.flatmatrix = [item for sublist in self.matrix for item in sublist]
 
-    def get_id(self, i, j):
-        return i * self.size + j
+    def get_zeroes(self):
+        zeroes = []
+        for i, val in enumerate(self.flatmatrix):
+            if val == 0:
+                zeroes.append(i)
+        return zeroes
 
-    def connect(self, i1, j1, i2, j2):
-        self.elements.union(self.get_id(i1, j1), self.get_id(i2, j2))
-
+    def connect_zeroes(self):
+       zeroes = []
+       for i, val in enumerate(self.flatmatrix):
+           if val == 0:
+               zeroes.append(i)
 
     def toString(self):
         graph = []
@@ -33,13 +38,11 @@ class Grid:
             graph.append(elements)
         print('\n'.join([' '.join([str(item) for item in row]) for row in graph]))
 
-if __name__ == '__main__':
-    np.random.seed(5)
-    grid = Grid(5)
-    grid.toString()
-    print(grid.matrix)
-    print(grid.elements.id)
-    print(grid.indices)
-    grid.connect_all()
 
-    
+if __name__ == '__main__':
+    grid = Grid(5)
+    print(grid.matrix)
+    print(grid.flatmatrix)
+    grid.toString()
+    zeroes = grid.get_zeroes()
+    print(zeroes)
